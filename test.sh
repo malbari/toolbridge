bcafa14cf706231672828a027811e20c8a2fecc0
Maurizio Albari <maurizio.albari@gmail.com>
2025-11-17 14:52:55 +0100

    fixed

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TOKEN="Bearer my-secret-token-123"
+TOKEN="my-secret-token-123"
 
 curl -X POST "http://localhost:4000/v1/chat/completions" \
  -H "Accept: application/json" \
  -H "Content-Type: application/json" \
- -H "Authorization: $TOKEN" \
+ -H "Authorization: Bearer $TOKEN" \
  -d '{"model":"velvet-14b","messages":[{"role":"user","content":"Test: 2 + 2 ?"}]}' \
 
